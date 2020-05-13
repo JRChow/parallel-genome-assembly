4a789267237f4be957b7a86dfb430cd5ecb65cb3
Jerome <zjrtomjohn@hotmail.com>
2020-05-12 23:17:52 -0700

    Update testing script

--- check_correct.sh
+++ check_correct.sh
@@ -3,7 +3,7 @@ cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=CC -DKMER_LEN=19 .. && cma
 DATA_DIR=$SCRATCH/my_datasets
 # DATA_DIR=$SCRATCH/my_datasets/smaller
 N_NODE=1
-N_PROC=68
+N_PROC=`expr $N_NODE \* 68`
 MODE=verbose
 salloc -N $N_NODE -A mp309 -t 10:00 -q debug --qos=interactive -C knl srun -N $N_NODE -n $N_PROD ./kmer_hash $DATA_DIR/test.txt $MODE
 if [ $MODE == "test" ]; then
--- check_human.sh
+++ check_human.sh
@@ -3,10 +3,10 @@ export UPCXX_SEGMENT_MB=4000
 export GASNET_MAX_SEGSIZE=4G
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=CC -DKMER_LEN=51 .. && cmake --build .
 DATA_DIR=$SCRATCH/my_datasets
-N_NODE=4
-N_PROC=272
+N_NODE=10
+N_PROC=`expr $N_NODE \* 68`
 MODE=verbose
-salloc -N $N_NODE -A mp309 -t 30:00 -q debug --qos=interactive -C knl srun -N $N_NODE -n $N_PROC ./kmer_hash $DATA_DIR/human-chr14-synthetic.txt $MODE
+salloc -N $N_NODE -A mp309 -t 10:00 -q debug --qos=interactive -C knl srun -N $N_NODE -n $N_PROC ./kmer_hash $DATA_DIR/human-chr14-synthetic.txt $MODE
 if [ $MODE == "test" ]; then
     echo "Checking correctness..."
     cat test*.dat | sort > my_solution.txt
